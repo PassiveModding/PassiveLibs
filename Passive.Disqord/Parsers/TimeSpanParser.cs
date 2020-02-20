39f5cabe7cd2398d73a558a0c1bfeda572bb4fd6
PassiveModding <passivemodding@gmail.com>
2020-02-20 11:42:00 +1100

    update timespan parser to support out of range expressions like 25h

--- Passive.Disqord/Parsers/TimeSpanParser.cs
+++ Passive.Disqord/Parsers/TimeSpanParser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Qmmands;
 
@@ -29,11 +31,87 @@ namespace Disqord.Extensions.Parsers
             "%s's'",                //      1s
         };
 
+        // TODO: Compile these expressions
+        private static readonly string TimeSpanExtendedCheck = @"[0-9dhms DHMS]";
+
+        private static readonly string TimeSpanDelimiter = @"(?<=[dhmsDHMS])";
+
+        private static readonly string TimeSpanDelimitedCheck = @"^[0-9]+[dhmsDHMS]$";
+
+        public ValueTask<TypeParserResult<TimeSpan>> ParseAsync(string input)
+        {
+            if (TimeSpan.TryParseExact(input.ToLowerInvariant(), Formats, CultureInfo.InvariantCulture, out var timeSpan))
+            {
+                return TypeParserResult<TimeSpan>.Successful(timeSpan);
+            }
+            else
+            {
+                // Attempt to parse values above what timespan regularly parses, ie. 24h => 1d is not normally possible.
+                if (!Regex.IsMatch(input, TimeSpanExtendedCheck))
+                {
+                    return TypeParserResult<TimeSpan>.Unsuccessful("Failed to parse TimeSpan");
+                }
+
+                // remove spaces.
+                input = input.Replace(" ", "");
+
+                // Split string into identified values ie. "6d24h" => { "6d", "24h" }, removing empty entries
+                var splits = Regex.Split(input, TimeSpanDelimiter).Where(x => x != string.Empty).ToArray();
+
+                // Somehow no results found.
+                if (splits.Length == 0)
+                {
+                    return TypeParserResult<TimeSpan>.Unsuccessful("Failed to parse TimeSpan");
+                }
+                else
+                {
+                    TimeSpan response = new TimeSpan();
+                    foreach (var split in splits)
+                    {
+                        // Split is only the delimiting character
+                        if (!Regex.IsMatch(split, TimeSpanDelimitedCheck))
+                        {
+                            return TypeParserResult<TimeSpan>.Unsuccessful($"Failed to parse TimeSpan (`{split}` is invalid.)");
+                        }
+
+                        var lastChar = split.Substring(split.Length - 1);
+                        var num = split.Substring(0, split.Length - 1);
+
+                        if (!double.TryParse(num, out var result))
+                        {
+                            return TypeParserResult<TimeSpan>.Unsuccessful($"Failed to parse TimeSpan (`{num}` is not of type `double`)");
+                        }
+                        switch (lastChar.ToLower())
+                        {
+                            case "d":
+                                response += TimeSpan.FromDays(result);
+                                break;
+
+                            case "h":
+                                response += TimeSpan.FromHours(result);
+                                break;
+
+                            case "m":
+                                response += TimeSpan.FromMinutes(result);
+                                break;
+
+                            case "s":
+                                response += TimeSpan.FromSeconds(result);
+                                break;
+
+                            default:
+                                return TypeParserResult<TimeSpan>.Unsuccessful($"Failed to parse TimeSpan (`{lastChar}` is not a valid timespan character.)");
+                        }
+                    }
+
+                    return TypeParserResult<TimeSpan>.Successful(response);
+                }
+            }
+        }
+
         public override ValueTask<TypeParserResult<TimeSpan>> ParseAsync(Parameter parameter, string input, CommandContext context)
         {
-            return TimeSpan.TryParseExact(input.ToLowerInvariant(), Formats, CultureInfo.InvariantCulture, out var timeSpan)
-                ? TypeParserResult<TimeSpan>.Successful(timeSpan)
-                : TypeParserResult<TimeSpan>.Unsuccessful("Failed to parse TimeSpan");
+            return ParseAsync(input);
         }
     }
 }
