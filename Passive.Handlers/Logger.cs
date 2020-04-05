8d286029f801656ffd77c41ac37a9085867e0f3e
PassiveModding <passivemodding@gmail.com>
2020-04-06 09:36:44 +1000

    Add Min Log Level filter

--- Passive.Handlers/Logger.cs
+++ Passive.Handlers/Logger.cs
@@ -4,25 +4,39 @@ namespace Passive
 {
     public class Logger
     {
+        public Logger(LogLevel minLogLevel)
+        {
+            MinLogLevel = minLogLevel;
+        }
+
         public enum LogLevel
         {
-            Info,
-            Error,
-            Verbose,
-            Debug,
-            Warn
+            Info = 2,
+
+            Error = 4,
+
+            Verbose = 0,
+
+            Debug = 1,
+
+            Warn = 3
         }
 
         public enum Source
         {
             Bot,
+
             Cmd
         }
+
+        public LogLevel MinLogLevel { get; set; } = LogLevel.Verbose;
 
         private string TimeStamp => DateTime.UtcNow.ToString("HH:mm:ss dd/MM/yy");
 
         public void Log(string message, string source, LogLevel level = LogLevel.Info)
         {
+            if (level < MinLogLevel) return;
+
             Console.WriteLine($"[{TimeStamp}]" +
                 $"[{level.ToString().ToUpper().PadRight(4).Substring(0, 4)}]" +
                 $"[{source.ToUpper()}]{message}");
