5029141dcf34aa4371a3ff366316571261381f23
PassiveModding <passivemodding@gmail.com>
2020-04-25 09:09:50 +1000

    Remove non generic url

--- Passive.Discord/Constants.cs
+++ Passive.Discord/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public static readonly string DeveloperApplicationLink = "https://discordapp.com/developers/applications/";
-        public static readonly string GithubApiCommitUrl = "https://api.github.com/repos/PassiveModding/Causym/commits";
+
         public static readonly string BotPermissionLevel = "8";
     }
 }
