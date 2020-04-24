888454bb7b2506996cf1c9d0a466d3616e557fae
PassiveModding <passivemodding@gmail.com>
2020-04-25 09:09:20 +1000

    Add timespan parsing

--- Passive.Extensions/Extensions.cs
+++ Passive.Extensions/Extensions.cs
@@ -37,6 +37,16 @@ namespace Passive
             return value;
         }
 
+        public static string GetReadableLength(this TimeSpan length)
+        {
+            int days = (int)length.TotalDays;
+            int hours = (int)length.TotalHours - days * 24;
+            int minutes = (int)length.TotalMinutes - days * 24 * 60 - hours * 60;
+            int seconds = (int)length.TotalSeconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+
+            return $"{(days > 0 ? $"{days} Day(s) " : "")}{(hours > 0 ? $"{hours} Hour(s) " : "")}{(minutes > 0 ? $"{minutes} Minute(s) " : "")}{(seconds > 0 ? $"{seconds} Second(s)" : "")}";
+        }
+
         /// <summary>
         /// Searches the specified assembly for all classes marked with the <see cref="Services.ServiceAttribute"/> attribute.
         /// </summary>
